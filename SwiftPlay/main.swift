d4d15b90130330b444c957508746525b48bb1aa0
aamiranwar <aamirr.anwarr@gmail.com>
2025-05-09 22:50:29 -0400

    remove element

--- SwiftPlay/main.swift
+++ SwiftPlay/main.swift
@@ -7,5 +7,22 @@
 
 import Foundation
 
-DataStructureCatalog.shared.testQueue()
-DataStructureCatalog.shared.testLinkedList()
+
+func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+    var k = 0
+    for i in 0..<nums.count {
+        if nums[i] != val {
+            nums[k] = nums[i]
+            k += 1
+        }
+    }
+    
+    return k
+}
+
+//var nums = [0,1,2,2,3,0,4,2]
+//print(removeElement(&nums, 2))
+
+var nums = [3,2,2,3]
+print(removeElement(&nums, 3))
+
